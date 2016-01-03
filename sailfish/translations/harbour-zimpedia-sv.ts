6fb7759c6b7e1ee7b737f2c0e9e2ca2affe5823a
Åke Engelbrektson <eson57@users.noreply.github.com>
2016-01-03 07:59:52 +0100

    Create harbour-zimpedia-sv.ts
    Swedish translation if wanted.

---
+++ sailfish/translations/harbour-zimpedia-sv.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="30"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="46"/>
+        <source>Version: %1</source>
+        <translation>Version: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="50"/>
+        <source>Offline Wikipedia and ZIM files viewer</source>
+        <translation>Offline Wikipedia och ZIM-filsläsare</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="55"/>
+        <source>Website</source>
+        <translation>Webbsida</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="60"/>
+        <source>Copyright &amp; license</source>
+        <translation>Copyright &amp; licens</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="72"/>
+        <source>This software is distributed under the terms of the Mozilla Public License v.2.0&lt;/a&gt;</source>
+        <translation>Denna mjukvara distribueras under vilkoren för Mozilla Public License v.2.0&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="77"/>
+        <source>View license</source>
+        <translation>Visa licensen</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="82"/>
+        <source>Third party components copyrights</source>
+        <translation>Copyright för tredjepartskomponenter</translation>
+    </message>
+</context>
+<context>
+    <name>Engine</name>
+    <message>
+        <location filename="../src/engine.cpp" line="71"/>
+        <source>No files to paste</source>
+        <translation>Inga filer att klistra in</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="80"/>
+        <source>Destination does not exist</source>
+        <translation>Platsen finns inte</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="90"/>
+        <source>Can&apos;t overwrite itself</source>
+        <translation>Kan inte skriva över sig själv</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="96"/>
+        <source>Can&apos;t move/copy to itself</source>
+        <translation>Kan inte flytta/kopiera till sig själv</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="213"/>
+        <source>File does not exist</source>
+        <translation>Filen finns inte</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="215"/>
+        <source>Broken symbolic link</source>
+        <translation>Trasig symbolisk länk</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="220"/>
+        <source>Can&apos;t read this type of file</source>
+        <translation>Kan inte läsa den här filtypen</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="225"/>
+        <source>No permission to read the file</source>
+        <translation>Läsbehörighet för filen saknas</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="229"/>
+        <location filename="../src/engine.cpp" line="235"/>
+        <source>Error reading file</source>
+        <translation>Kunde inte läsa filen</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="238"/>
+        <source>Empty file</source>
+        <translation>Tom fil</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="264"/>
+        <location filename="../src/engine.cpp" line="265"/>
+        <source>--- Binary file preview clipped at %1 kB ---</source>
+        <translation>--- Förhandsgranskning av binärfil begränsad till %1 kB ---</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="284"/>
+        <source>--- Text file preview clipped at %1 lines ---</source>
+        <translation>--- Förhandsgranskning av textfil begränsad till %1 rader ---</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="286"/>
+        <source>--- Text file preview clipped at %1 kB ---</source>
+        <translation>--- Förhandsgranskning av textfil begränsad till %1 kB ---</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="297"/>
+        <source>No permissions to create %1</source>
+        <translation>Inga rättigheter att skapa %1</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="299"/>
+        <source>Cannot create folder %1</source>
+        <translation>Kan inte skapa mappen %1</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="315"/>
+        <source>Cannot rename %1</source>
+        <translation>Kan inte byta namn på %1</translation>
+    </message>
+    <message>
+        <location filename="../src/engine.cpp" line="338"/>
+        <source>Cannot change permissions</source>
+        <translation>Kan inte ändra rättigheter</translation>
+    </message>
+</context>
+<context>
+    <name>FilesPage</name>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="31"/>
+        <source>Choose ZIM file</source>
+        <translation>Välj ZIM-fil</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="59"/>
+        <source>No files were found</source>
+        <translation>Inga filer hittades</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="60"/>
+        <source>Upload ZIM files to any folder under your home directory.</source>
+        <translation>Ladda upp ZIM-filer till valfri mapp under din hemkatalog.</translation>
+    </message>
+</context>
+<context>
+    <name>PageMenu</name>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="20"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="25"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="31"/>
+        <source>Change ZIM file</source>
+        <translation>Byt ZIM-fil</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="66"/>
+        <source>Find article, by typing in the Search field below</source>
+        <translation>Hitta artikeln genom att skriva i nedanstående sökfält</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="92"/>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="40"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="48"/>
+        <source>Enter port number here!</source>
+        <translation>Ange portnummer här!</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="49"/>
+        <source>Local server listening port number</source>
+        <translation>Lyssningsport för lokal server</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="66"/>
+        <source>Port number is invalid! Allowed port range is 1024-65535.</source>
+        <translation>Portnummret är ogiltigt! Giltigt intervall är 1024-65535.</translation>
+    </message>
+</context>
+</TS>
