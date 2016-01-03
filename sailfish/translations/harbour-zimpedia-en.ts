d5c7c55da090d984593dd909e08ecf61e1475e5a
mkiol <michal@mkiol.net>
2016-01-03 15:33:14 +0100

    pl and sv translations update

--- sailfish/translations/harbour-zimpedia-en.ts
+++ sailfish/translations/harbour-zimpedia-en.ts
@@ -15,7 +15,7 @@
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="50"/>
-        <source>Offline Wikipedia and ZIM files viewer</source>
+        <source>Offline reader for Wikipedia and other Wiki projects</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -30,107 +30,12 @@
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="72"/>
-        <source>This software is distributed under the terms of the Mozilla Public License v.2.0&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/AboutPage.qml" line="77"/>
-        <source>View license</source>
+        <source>This software is distributed under the terms of the Mozilla Public License v.2.0</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="82"/>
         <source>Third party components copyrights</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>Engine</name>
-    <message>
-        <location filename="../src/engine.cpp" line="71"/>
-        <source>No files to paste</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="80"/>
-        <source>Destination does not exist</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="90"/>
-        <source>Can&apos;t overwrite itself</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="96"/>
-        <source>Can&apos;t move/copy to itself</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="213"/>
-        <source>File does not exist</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="215"/>
-        <source>Broken symbolic link</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="220"/>
-        <source>Can&apos;t read this type of file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="225"/>
-        <source>No permission to read the file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="229"/>
-        <location filename="../src/engine.cpp" line="235"/>
-        <source>Error reading file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="238"/>
-        <source>Empty file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="264"/>
-        <location filename="../src/engine.cpp" line="265"/>
-        <source>--- Binary file preview clipped at %1 kB ---</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="284"/>
-        <source>--- Text file preview clipped at %1 lines ---</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="286"/>
-        <source>--- Text file preview clipped at %1 kB ---</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="297"/>
-        <source>No permissions to create %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="299"/>
-        <source>Cannot create folder %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="315"/>
-        <source>Cannot rename %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="338"/>
-        <source>Cannot change permissions</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -142,13 +47,13 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/FilesPage.qml" line="59"/>
+        <location filename="../qml/FilesPage.qml" line="58"/>
         <source>No files were found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/FilesPage.qml" line="60"/>
-        <source>Upload ZIM files to any folder under your home directory.</source>
+        <location filename="../qml/FilesPage.qml" line="63"/>
+        <source>The ZIM is an open file format that stores wiki content for offline usage. The collection of nice wikis can be downloaded from &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;this page&lt;/a&gt;. If you already have some ZIM files, put them to any folder you like under your home directory or SD card.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -174,7 +79,7 @@
     <name>SearchPage</name>
     <message>
         <location filename="../qml/SearchPage.qml" line="66"/>
-        <source>Find article, by typing in the Search field below</source>
+        <source>Find article, by typing in the search field</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
--- sailfish/translations/harbour-zimpedia-pl.ts
+++ sailfish/translations/harbour-zimpedia-pl.ts
@@ -1,137 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="pl_PL" sourcelanguage="en">
 <context>
     <name>AboutPage</name>
     <message>
         <location filename="../qml/AboutPage.qml" line="30"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>O Zimpedii</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="46"/>
         <source>Version: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Wersja %1</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="50"/>
-        <source>Offline Wikipedia and ZIM files viewer</source>
-        <translation type="unfinished"></translation>
+        <source>Offline reader for Wikipedia and other Wiki projects</source>
+        <translation>Niewymagający dostępu do Internetu, czytnik Wikipedii oraz innych projektów typu Wiki</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="55"/>
         <source>Website</source>
-        <translation type="unfinished"></translation>
+        <translation>Strona internetowa</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="60"/>
         <source>Copyright &amp; license</source>
-        <translation type="unfinished"></translation>
+        <translation>Prawa autorskie i licencja</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="72"/>
-        <source>This software is distributed under the terms of the Mozilla Public License v.2.0&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <source>This software is distributed under the terms of the Mozilla Public License v.2.0</source>
+        <translation>Oprogramowanie dystrybuowane zgodnie z postanowieniami licencji Mozilla Public License w wersji 2.0</translation>
     </message>
     <message>
-        <location filename="../qml/AboutPage.qml" line="77"/>
-        <source>View license</source>
-        <translation type="unfinished"></translation>
+        <source>This software is distributed under the terms of the Mozilla Public License v.2.0&lt;/a&gt;</source>
+        <translation type="vanished">Oprogramowanie dystrybuowane zgodnie z postanowieniami licencji Mozilla Public License w wersji 2.0.</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="82"/>
         <source>Third party components copyrights</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>Engine</name>
-    <message>
-        <location filename="../src/engine.cpp" line="71"/>
-        <source>No files to paste</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="80"/>
-        <source>Destination does not exist</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="90"/>
-        <source>Can&apos;t overwrite itself</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="96"/>
-        <source>Can&apos;t move/copy to itself</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="213"/>
-        <source>File does not exist</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="215"/>
-        <source>Broken symbolic link</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="220"/>
-        <source>Can&apos;t read this type of file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="225"/>
-        <source>No permission to read the file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="229"/>
-        <location filename="../src/engine.cpp" line="235"/>
-        <source>Error reading file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="238"/>
-        <source>Empty file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="264"/>
-        <location filename="../src/engine.cpp" line="265"/>
-        <source>--- Binary file preview clipped at %1 kB ---</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="284"/>
-        <source>--- Text file preview clipped at %1 lines ---</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="286"/>
-        <source>--- Text file preview clipped at %1 kB ---</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="297"/>
-        <source>No permissions to create %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="299"/>
-        <source>Cannot create folder %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="315"/>
-        <source>Cannot rename %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/engine.cpp" line="338"/>
-        <source>Cannot change permissions</source>
-        <translation type="unfinished"></translation>
+        <translation>Prawa autorskie komponentów stron trzecich</translation>
     </message>
 </context>
 <context>
@@ -139,17 +48,21 @@
     <message>
         <location filename="../qml/FilesPage.qml" line="31"/>
         <source>Choose ZIM file</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz plik ZIM</translation>
     </message>
     <message>
-        <location filename="../qml/FilesPage.qml" line="59"/>
+        <location filename="../qml/FilesPage.qml" line="58"/>
         <source>No files were found</source>
-        <translation type="unfinished"></translation>
+        <translation>Brak plików</translation>
     </message>
     <message>
-        <location filename="../qml/FilesPage.qml" line="60"/>
-        <source>Upload ZIM files to any folder under your home directory.</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/FilesPage.qml" line="63"/>
+        <source>The ZIM is an open file format that stores wiki content for offline usage. The collection of nice wikis can be downloaded from &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;this page&lt;/a&gt;. If you already have some ZIM files, put them to any folder you like under your home directory or SD card.</source>
+        <translation>ZIM jest otwartym formatem umożliwiającym przechowywanie i korzystanie ze stron typu Wiki bez dostępu do Internetu. Zbiór ciekawych Wiki można pobrać z &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;tej strony&lt;/a&gt;. Jeśli już masz pliki w formacie ZIM, umieść je w dowolnym folderze znajdującym się w katalogu domowym lub na karcie SD.</translation>
+    </message>
+    <message>
+        <source>The ZIM is an open file format that stores wiki content for offline usage. The collection of nice wikis can be downloaded from &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;this page&lt;/a&gt;. If you already have some ZIM files, put them to any folder you like under your home directory.</source>
+        <translation type="vanished">ZIM jest otwartym formatem umożliwiającym przechowywanie i korzystanie ze stron typu Wiki bez dostępu do Internetu. Zbiór ciekawych Wiki można pobrać z &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;tej strony&lt;/a&gt;. Jeśli już masz pliki w formacie ZIM, umieść je w dowolnym folderze znajdującym się w katalogu domowym lub na karcie SD.</translation>
     </message>
 </context>
 <context>
@@ -157,30 +70,30 @@
     <message>
         <location filename="../qml/PageMenu.qml" line="20"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>O Zimpedii</translation>
     </message>
     <message>
         <location filename="../qml/PageMenu.qml" line="25"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia</translation>
     </message>
     <message>
         <location filename="../qml/PageMenu.qml" line="31"/>
         <source>Change ZIM file</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmień plik ZIM</translation>
     </message>
 </context>
 <context>
     <name>SearchPage</name>
     <message>
         <location filename="../qml/SearchPage.qml" line="66"/>
-        <source>Find article, by typing in the Search field below</source>
-        <translation type="unfinished"></translation>
+        <source>Find article, by typing in the search field</source>
+        <translation>Znajdź artykuł, wpisując szukane słowo</translation>
     </message>
     <message>
         <location filename="../qml/SearchPage.qml" line="92"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Szukaj</translation>
     </message>
 </context>
 <context>
@@ -188,22 +101,22 @@
     <message>
         <location filename="../qml/SettingsPage.qml" line="40"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="48"/>
         <source>Enter port number here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Wpisz tutaj numer portu!</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="49"/>
         <source>Local server listening port number</source>
-        <translation type="unfinished"></translation>
+        <translation>Numer portu na którym będzie słuchał lokalny serwer HTTP</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="66"/>
         <source>Port number is invalid! Allowed port range is 1024-65535.</source>
-        <translation type="unfinished"></translation>
+        <translation>Numer portu nie jest prawidłowy. Prawidłowy zakres to: 1024-65535.</translation>
     </message>
 </context>
 </TS>
