d7e2ef618dd7cf9bb0fe524fab20456dedd5e525
Dominik Chrástecký <dominik@chrastecky.eu>
2016-01-03 19:40:08 +0100

    Added czech translation

---
+++ sailfish/translations/harbour-zimpedia-cs_CZ.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="30"/>
+        <source>About</source>
+        <translation>O aplikaci</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="46"/>
+        <source>Version: %1</source>
+        <translation>Verze: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="50"/>
+        <source>Offline reader for Wikipedia and other Wiki projects</source>
+        <translation>Offline čtečka pro Wikipedia a jiné Wiki projekty</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="55"/>
+        <source>Website</source>
+        <translation>Web</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="60"/>
+        <source>Copyright &amp; license</source>
+        <translation>Copyright a licence</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="72"/>
+        <source>This software is distributed under the terms of the Mozilla Public License v.2.0</source>
+        <translation>Tento software je distribuován pod licencí Mozilla Public License v2.0</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="82"/>
+        <source>Third party components copyrights</source>
+        <translation>Copyright kompnentů třetích stran</translation>
+    </message>
+</context>
+<context>
+    <name>FilesPage</name>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="31"/>
+        <source>Choose ZIM file</source>
+        <translation>Zvolte ZIM soubor</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="58"/>
+        <source>No files were found</source>
+        <translation>Žádné soubory nenalezeny</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="63"/>
+        <source>The ZIM is an open file format that stores wiki content for offline usage. The collection of nice wikis can be downloaded from &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;this page&lt;/a&gt;. If you already have some ZIM files, put them to any folder you like under your home directory or SD card.</source>
+        <translation>ZIM je otevřený formát, který ukládá obsah wiki pro offline použití. Kolekce různých wiki může být stažena z &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;této stránky&lt;/a&gt;. Pokud už nějaké ZIM soubory máte, dejte je do jakékoliv složky ve vaší domovské složce, nebo na SD kartě.</translation>
+    </message>
+</context>
+<context>
+    <name>PageMenu</name>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="20"/>
+        <source>About</source>
+        <translation>O aplikaci</translation>
+    </message>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="25"/>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="31"/>
+        <source>Change ZIM file</source>
+        <translation>Změnit ZIM soubor</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="66"/>
+        <source>Find article, by typing in the search field</source>
+        <translation>Vyhledejte článek psaním do vyhledávacího pole</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="92"/>
+        <source>Search</source>
+        <translation>Hledat</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="40"/>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="48"/>
+        <source>Enter port number here!</source>
+        <translation>Vložte číslo portu!</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="49"/>
+        <source>Local server listening port number</source>
+        <translation>Lokální server naslouchá na portu</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="66"/>
+        <source>Port number is invalid! Allowed port range is 1024-65535.</source>
+        <translation>Číslo portu je neplatné! Povolený rozsah je 1024 - 65535.</translation>
+    </message>
+</context>
+</TS>
