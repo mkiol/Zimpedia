f332bed7e789fa972cfcb68bf9cd4e73341bd67a
Carmen F. B <carmenfdezb@yahoo.es>
2016-01-04 18:30:12 +0100

    Added Spanish translation

---
+++ sailfish/translations/harbour-zimpedia-es.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="30"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="46"/>
+        <source>Version: %1</source>
+        <translation>Versión: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="50"/>
+        <source>Offline reader for Wikipedia and other Wiki projects</source>
+        <translation>Lector sin conexión de Wikipedia y otros proyectos Wiki</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="55"/>
+        <source>Website</source>
+        <translation>Sitio Web</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="60"/>
+        <source>Copyright &amp; license</source>
+        <translation>Copyright y licencia</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="72"/>
+        <source>This software is distributed under the terms of the Mozilla Public License v.2.0</source>
+        <translation>Este software se distribuye bajo los términos de la Licencia Pública de Mozilla v.2.0</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="82"/>
+        <source>Third party components copyrights</source>
+        <translation>Copyrights de componentes de terceros</translation>
+    </message>
+</context>
+<context>
+    <name>FilesPage</name>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="31"/>
+        <source>Choose ZIM file</source>
+        <translation>Elegir fichero ZIM</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="58"/>
+        <source>No files were found</source>
+        <translation>No se ha encontrado ningún fichero</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="63"/>
+        <source>The ZIM is an open file format that stores wiki content for offline usage. The collection of nice wikis can be downloaded from &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;this page&lt;/a&gt;. If you already have some ZIM files, put them to any folder you like under your home directory or SD card.</source>
+        <translation>ZIM es un formato de archivo abierto que almacena el contenido wiki para usarlo sin conexión. La colección de buenos wikis pueden descargarse desde &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt; esta página &lt;/a&gt;. Si ya dispones de algunos ficheros ZIM, ubícalos en cualquier carpeta dentro del directorio Home o en la tarjeta SD.</translation>
+    </message>
+</context>
+<context>
+    <name>PageMenu</name>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="20"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="25"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../qml/PageMenu.qml" line="31"/>
+        <source>Change ZIM file</source>
+        <translation>Cambiar fichero ZIM</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="66"/>
+        <source>Find article, by typing in the search field</source>
+        <translation>Buscar artículo, escribiendo en el campo de búsqueda</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="92"/>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="40"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="48"/>
+        <source>Enter port number here!</source>
+        <translation>¡Introduce el puerto aquí!</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="49"/>
+        <source>Local server listening port number</source>
+        <translation>El servidor local está escuchando en el puerto</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="66"/>
+        <source>Port number is invalid! Allowed port range is 1024-65535.</source>
+        <translation>¡El puerto no es correcto! El rango de puertos permitido es 1024-65535.</translation>
+    </message>
+</context>
+</TS>
