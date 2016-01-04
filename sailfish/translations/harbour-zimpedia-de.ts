d06ad8b581432fda51999f4128f0b875505f38a0
popanz <popanz@online.de>
2016-01-04 16:30:35 +0100

    Update and rename harbour-zimpedia-pl.ts to harbour-zimpedia-de.ts

--- sailfish/translations/harbour-zimpedia-de.ts
+++ sailfish/translations/harbour-zimpedia-de.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pl_PL" sourcelanguage="en">
+<TS version="2.1" language="de_DE" sourcelanguage="en">
 <context>
     <name>AboutPage</name>
     <message>
@@ -11,36 +11,36 @@
     <message>
         <location filename="../qml/AboutPage.qml" line="46"/>
         <source>Version: %1</source>
-        <translation>Wersja %1</translation>
+        <translation>Version %1</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="50"/>
         <source>Offline reader for Wikipedia and other Wiki projects</source>
-        <translation>Niewymagający dostępu do Internetu, czytnik Wikipedii oraz innych projektów typu Wiki</translation>
+        <translation>Offline Reader für die Wikipedia und andere Wiki-Projekte</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="55"/>
         <source>Website</source>
-        <translation>Strona internetowa</translation>
+        <translation>Webseite</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="60"/>
         <source>Copyright &amp; license</source>
-        <translation>Prawa autorskie i licencja</translation>
+        <translation>Copyright und Lizenz</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="72"/>
         <source>This software is distributed under the terms of the Mozilla Public License v.2.0</source>
-        <translation>Oprogramowanie dystrybuowane zgodnie z postanowieniami licencji Mozilla Public License w wersji 2.0</translation>
+        <translation>Diese Software wird unter den Bestimmungen der Mozilla Public License v.2.0 vertrieben</translation>
     </message>
     <message>
         <source>This software is distributed under the terms of the Mozilla Public License v.2.0&lt;/a&gt;</source>
-        <translation type="vanished">Oprogramowanie dystrybuowane zgodnie z postanowieniami licencji Mozilla Public License w wersji 2.0.</translation>
+        <translation type="vanished">Diese Software wird unter den Bestimmungen der Mozilla Public License v.2.0</translation>
     </message>
     <message>
         <location filename="../qml/AboutPage.qml" line="82"/>
         <source>Third party components copyrights</source>
-        <translation>Prawa autorskie komponentów stron trzecich</translation>
+        <translation>Copyright-Bestandteile von Dritten</translation>
     </message>
 </context>
 <context>
@@ -48,21 +48,21 @@
     <message>
         <location filename="../qml/FilesPage.qml" line="31"/>
         <source>Choose ZIM file</source>
-        <translation>Wybierz plik ZIM</translation>
+        <translation>Wähle eine ZIM Datei</translation>
     </message>
     <message>
         <location filename="../qml/FilesPage.qml" line="58"/>
         <source>No files were found</source>
-        <translation>Brak plików</translation>
+        <translation>Es wurden keine Dateien gefunden</translation>
     </message>
     <message>
         <location filename="../qml/FilesPage.qml" line="63"/>
         <source>The ZIM is an open file format that stores wiki content for offline usage. The collection of nice wikis can be downloaded from &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;this page&lt;/a&gt;. If you already have some ZIM files, put them to any folder you like under your home directory or SD card.</source>
-        <translation>ZIM jest otwartym formatem umożliwiającym przechowywanie i korzystanie ze stron typu Wiki bez dostępu do Internetu. Zbiór ciekawych Wiki można pobrać z &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;tej strony&lt;/a&gt;. Jeśli już masz pliki w formacie ZIM, umieść je w dowolnym folderze znajdującym się w katalogu domowym lub na karcie SD.</translation>
+        <translation>ZIM ist ein Dateiformat, welches Wiki Inhalte für die Offline_Nutzung enthält. Eine Auswahl von tollen Wikis can hier heruntergeladen werden: &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;tej strony&lt;/a&gt;. Solltest Du schon einige ZIM Dateien haben, speichere sie in einem beliebigen Ordner Deines Home-Verzeichnisses oder der SD-Karte.</translation>
     </message>
     <message>
         <source>The ZIM is an open file format that stores wiki content for offline usage. The collection of nice wikis can be downloaded from &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;this page&lt;/a&gt;. If you already have some ZIM files, put them to any folder you like under your home directory.</source>
-        <translation type="vanished">ZIM jest otwartym formatem umożliwiającym przechowywanie i korzystanie ze stron typu Wiki bez dostępu do Internetu. Zbiór ciekawych Wiki można pobrać z &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;tej strony&lt;/a&gt;. Jeśli już masz pliki w formacie ZIM, umieść je w dowolnym folderze znajdującym się w katalogu domowym lub na karcie SD.</translation>
+        <translation type="vanished">ZIM ist ein Dateiformat, welches Wiki Inhalte für die Offline-Nutzung enthält. Eine Auswahl von tollen Wikis can hier heruntergeladen werden: &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;tej strony&lt;/a&gt;. Solltest Du schon einige ZIM Dateien haben, speichere sie in einem beliebigen Ordner Deines Home-Verzeichnisses oder der SD-Karte.</translation>
     </message>
 </context>
 <context>
@@ -70,17 +70,17 @@
     <message>
         <location filename="../qml/PageMenu.qml" line="20"/>
         <source>About</source>
-        <translation>O Zimpedii</translation>
+        <translation>Über...</translation>
     </message>
     <message>
         <location filename="../qml/PageMenu.qml" line="25"/>
         <source>Settings</source>
-        <translation>Ustawienia</translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../qml/PageMenu.qml" line="31"/>
         <source>Change ZIM file</source>
-        <translation>Zmień plik ZIM</translation>
+        <translation>Wechsle die ZIM Datei</translation>
     </message>
 </context>
 <context>
@@ -88,12 +88,12 @@
     <message>
         <location filename="../qml/SearchPage.qml" line="66"/>
         <source>Find article, by typing in the search field</source>
-        <translation>Znajdź artykuł, wpisując szukane słowo</translation>
+        <translation>Finde Artikel über die Eingabe im Suchfeld</translation>
     </message>
     <message>
         <location filename="../qml/SearchPage.qml" line="92"/>
         <source>Search</source>
-        <translation>Szukaj</translation>
+        <translation>Suche</translation>
     </message>
 </context>
 <context>
@@ -101,22 +101,23 @@
     <message>
         <location filename="../qml/SettingsPage.qml" line="40"/>
         <source>Settings</source>
-        <translation>Ustawienia</translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="48"/>
         <source>Enter port number here!</source>
-        <translation>Wpisz tutaj numer portu!</translation>
+        <translation>Gib die Port-Nummer hier ein!</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="49"/>
         <source>Local server listening port number</source>
-        <translation>Numer portu na którym będzie słuchał lokalny serwer HTTP</translation>
+        <translation>Empfangende Port-Nummer des lokalen Servers</translation>
     </message>
     <message>
         <location filename="../qml/SettingsPage.qml" line="66"/>
         <source>Port number is invalid! Allowed port range is 1024-65535.</source>
-        <translation>Numer portu nie jest prawidłowy. Prawidłowy zakres to: 1024-65535.</translation>
+        <translation>Die Port-Nummer ist ungültig! Erlaubter Bereich ist 1024-65535.</translation>
     </message>
 </context>
 </TS>
+This software is distributed under the terms of the Mozilla Public License v.2.0
