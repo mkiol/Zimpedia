432532f03cfb31632c7d6bfb7c253c6cd953363e
Frans de Jonge <fransdejonge@gmail.com>
2019-02-03 21:31:35 +0100

    [i18n] Add Dutch translation

---
+++ sailfish/translations/Zimpedia_nl.ts
@@ -0,0 +1,466 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="31"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="55"/>
+        <source>Changelog</source>
+        <translation>Changelog</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="61"/>
+        <source>Project website</source>
+        <translation>Projectwebsite</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="67"/>
+        <source>Authors</source>
+        <translation>Auteurs</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="80"/>
+        <source>Translations are provided by:</source>
+        <translation>Vertalingen voorzien door:</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="92"/>
+        <source>%1 is developed as an open source project under %2.</source>
+        <translation>%1 is ontwikkeld als een open sourceproject onder %2.</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="98"/>
+        <source>Libraries</source>
+        <translation>Bibliotheken</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="47"/>
+        <source>Version %1</source>
+        <translation>Versie %1</translation>
+    </message>
+</context>
+<context>
+    <name>BookmarkEditPage</name>
+    <message>
+        <location filename="../qml/BookmarkEditPage.qml" line="44"/>
+        <source>Save</source>
+        <translation>Opslaan</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkEditPage.qml" line="53"/>
+        <location filename="../qml/BookmarkEditPage.qml" line="54"/>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkEditPage.qml" line="80"/>
+        <source>Change</source>
+        <translation>Wijzigen</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkEditPage.qml" line="118"/>
+        <source>ZIM file is missing</source>
+        <translation>Zimbestand ontbreekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkEditPage.qml" line="180"/>
+        <source>Something went wrong and bookmark cannot be updated</source>
+        <translation>Er is iets misgegaan en de bladwijzer kan niet worden bijgewerkt</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkEditPage.qml" line="153"/>
+        <source>Copy URL</source>
+        <translation>Kopieer url</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkEditPage.qml" line="155"/>
+        <source>URL was copied to the clipboard</source>
+        <translation>Url gekopieerd naar klembord</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkEditPage.qml" line="127"/>
+        <source>URL</source>
+        <translation>Url</translation>
+    </message>
+</context>
+<context>
+    <name>BookmarkFileChoose</name>
+    <message>
+        <location filename="../qml/BookmarkFileChoose.qml" line="32"/>
+        <source>Choose ZIM file</source>
+        <translation>Kies Zimbestand</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkFileChoose.qml" line="103"/>
+        <source>No files were found</source>
+        <translation>Geen bestanden gevonden</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarkFileChoose.qml" line="108"/>
+        <source>Refresh</source>
+        <translation>Verversen</translation>
+    </message>
+</context>
+<context>
+    <name>BookmarksPage</name>
+    <message>
+        <location filename="../qml/BookmarksPage.qml" line="35"/>
+        <source>Bookmarks</source>
+        <translation>Bladwijzers</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarksPage.qml" line="47"/>
+        <source>Edit</source>
+        <translation>Bewerken</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarksPage.qml" line="62"/>
+        <source>Delete</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarksPage.qml" line="113"/>
+        <source>ZIM file is missing</source>
+        <translation>Zimbestand ontbreekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarksPage.qml" line="122"/>
+        <source>Bookmark&apos;s ZIM file is missing</source>
+        <translation>Bladwijzerzimbestand ontbreekt</translation>
+    </message>
+    <message>
+        <location filename="../qml/BookmarksPage.qml" line="128"/>
+        <source>No bookmarks</source>
+        <translation>Geen bladwijzers</translation>
+    </message>
+</context>
+<context>
+    <name>ChangelogPage</name>
+    <message>
+        <location filename="../qml/ChangelogPage.qml" line="30"/>
+        <source>Changelog</source>
+        <translation>Changelog</translation>
+    </message>
+    <message>
+        <location filename="../qml/ChangelogPage.qml" line="34"/>
+        <location filename="../qml/ChangelogPage.qml" line="52"/>
+        <location filename="../qml/ChangelogPage.qml" line="66"/>
+        <source>Version %1</source>
+        <translation>Versie %1</translation>
+    </message>
+</context>
+<context>
+    <name>FilesPage</name>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="57"/>
+        <source>Show details</source>
+        <translation>Details weergeven</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="158"/>
+        <source>No files were found</source>
+        <translation>Geen bestanden gevonden</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="122"/>
+        <source>The ZIM is an open file format that stores wiki content for offline usage. The collection of nice wikis can be downloaded from &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;this page&lt;/a&gt;. If you already have some ZIM files, put them to any folder you like under your home directory or SD card.</source>
+        <translation>Zim is een open bestandsformaat dat wiki-inhoud opslaat voor offline gebruik. Een collectie mooie wiki&apos;s kan worden gedownload van &lt;a href=&apos;http://www.kiwix.org/wiki/Content_in_all_languages&apos;&gt;deze pagina&lt;/a&gt;. Als je reeds enkele zimbestanden hebt, kun je ze in om het even welke map onder je thuismap of SD-kaart plaatsen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="35"/>
+        <source>ZIM files</source>
+        <translation>Zimbestanden</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="49"/>
+        <source>Open file</source>
+        <translation>Open bestand</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="132"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="138"/>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="144"/>
+        <source>Bookmarks</source>
+        <translation>Bladwijzers</translation>
+    </message>
+    <message>
+        <location filename="../qml/FilesPage.qml" line="151"/>
+        <source>Refresh</source>
+        <translation>Verversen</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="65"/>
+        <source>Open in built-in viewer</source>
+        <translation>Openen met ingebouwde weergave</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="65"/>
+        <source>Open in browser</source>
+        <translation>Openen in browser</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="77"/>
+        <source>Add to bookmarks</source>
+        <translation>Bladwijzer toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="165"/>
+        <source>No articles</source>
+        <translation>Geen artikels</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="166"/>
+        <source>Find article, by typing in the search field</source>
+        <translation>Vind een artikel door in het zoekveld te typen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="98"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="103"/>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="108"/>
+        <source>Change ZIM file</source>
+        <translation>Zimbestand kiezen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="113"/>
+        <source>Bookmarks</source>
+        <translation>Bladwijzers</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="119"/>
+        <source>Open main page</source>
+        <translation>Hoofdpagina openen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SearchPage.qml" line="37"/>
+        <source>Search</source>
+        <translation>Zoeken</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="40"/>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="44"/>
+        <source>Search mode</source>
+        <translation>Zoekmodus</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="45"/>
+        <source>By default when you enter a phase, search only by article title is performed. A different mode is a search within full article text. The full-text search mode works only when ZIM file contains appropriate index.</source>
+        <translation>Standaard wordt er enkel op artikeltitel gezocht bij het ingeven van een zoekterm. Er is ook een modus om in de volledige artikeltekst te zoeken. De volledigetekstmodus werkt alleen als het zimbestand een geschikte index heeft.</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="50"/>
+        <source>By title</source>
+        <translation>Op titel</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="51"/>
+        <source>Full-text</source>
+        <translation>Volledige tekst</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="66"/>
+        <source>Web view</source>
+        <translation>Webview</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="62"/>
+        <source>Default viewer</source>
+        <translation>Standaardweergave</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="67"/>
+        <source>Browser</source>
+        <translation>Browser</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="74"/>
+        <source>Wiki pages can be opened in the built-in web viewer or in an external browser.</source>
+        <translation>Wikipagina&apos;s kunnen worden geopend in de ingebouwde webviewer of in een externe browser.</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsPage.qml" line="80"/>
+        <source>Viewer font size level</source>
+        <translation>Viewer lettertypegrootte</translation>
+    </message>
+</context>
+<context>
+    <name>WebViewPage</name>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="44"/>
+        <location filename="../qml/WebViewPage.qml" line="261"/>
+        <source>Launching an external browser...</source>
+        <translation>Externe browser wordt gestart…</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="162"/>
+        <source>Loading page content...</source>
+        <translation>Pagina-inhoud wordt geladen…</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="170"/>
+        <source>Failed to load page content</source>
+        <translation>Pagina-inhoud laden mislukt</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="226"/>
+        <source>Back</source>
+        <translation>Terug</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="233"/>
+        <source>Toggle Night View</source>
+        <translation>Nachtweergave schakelen</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="244"/>
+        <source>Add to bookmarks</source>
+        <translation>Bladwijzer toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="257"/>
+        <source>Browser</source>
+        <translation>Browser</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="268"/>
+        <source>Decrease font</source>
+        <translation>Lettertype kleiner maken</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="277"/>
+        <source>Increase font</source>
+        <translation>Lettertype groter maken</translation>
+    </message>
+    <message>
+        <location filename="../qml/WebViewPage.qml" line="286"/>
+        <source>Hide toolbar</source>
+        <translation>Werkbalk verbergen</translation>
+    </message>
+</context>
+<context>
+    <name>ZimInfoPage</name>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="45"/>
+        <source>Details</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="62"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="67"/>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="72"/>
+        <source>Date</source>
+        <translation>Datum</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="77"/>
+        <source>Creator</source>
+        <translation>Maker</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="82"/>
+        <source>Publisher</source>
+        <translation>Uitgever</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="87"/>
+        <source>Description</source>
+        <translation>Omschrijving</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="92"/>
+        <source>Language</source>
+        <translation>Taal</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="102"/>
+        <source>Full-text index</source>
+        <translation>Volledige tekstindex</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="103"/>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="103"/>
+        <source>No</source>
+        <translation>Nee</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="97"/>
+        <source>Article count</source>
+        <translation>Aantal artikelen</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="112"/>
+        <source>Path</source>
+        <translation>Pad</translation>
+    </message>
+    <message>
+        <location filename="../qml/ZimInfoPage.qml" line="107"/>
+        <source>Size</source>
+        <translation>Grootte</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="44"/>
+        <source>Launching an external browser...</source>
+        <translation>Externe browser wordt gestart…</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="56"/>
+        <source>Bookmark has been added</source>
+        <translation>Bladwijzer toegevoegd</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="57"/>
+        <source>Bookmark already exists</source>
+        <translation>Bladwijzer bestaat al</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="59"/>
+        <source>Bookmark has been updated</source>
+        <translation>Bladwijzer bijgewerkt</translation>
+    </message>
+</context>
+</TS>
